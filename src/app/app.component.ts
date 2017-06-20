4eead82dfe7ad33ec57b1efd4c631e3001c9f80f
Wagner Barbosa <wsoubar@gmail.com>
2017-06-20 00:43:30 -0300

    exemplos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from "rxjs/Observable";
+import * as firebase from 'firebase/app';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+
+    user: Observable<firebase.User>;
+    items: FirebaseListObservable<any[]>;
+    msgVal: string = '';
+
+    constructor(public afAuth: AngularFireAuth, public db: AngularFireDatabase) {
+        this.items = db.list('/messages', {
+            query: {
+                limitToLast: 50
+            }
+        });
+
+        this.user = this.afAuth.authState;
+
+    }
+    login() {
+        this.afAuth.auth.signInAnonymously();
+    }
+
+    logout() {
+        this.afAuth.auth.signOut();
+    }
+    
+    Send(desc: string) {
+        this.items.push({ message: desc });
+        this.msgVal = '';
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+export const firebaseConfig = {
+      apiKey: "AIzaSyDOypC4jUFWbpt973rkH9k8geIZBcE68IY",
+      authDomain: "cursoonline-e9104.firebaseapp.com",
+      databaseURL: "https://cursoonline-e9104.firebaseio.com",
+      projectId: "cursoonline-e9104",
+      storageBucket: "cursoonline-e9104.appspot.com",
+      messagingSenderId: "184461736739"
+};  
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
